9e7da7477b4d62433d6263e5351a1c1f47c5e7e0
alberto <asoragna@irobot.com>
2018-11-07 19:29:35 -0800

    remove not needed paths in cmake

--- ros2_ws/cross_compile.sh
+++ ros2_ws/cross_compile.sh
@@ -15,9 +15,5 @@ colcon \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
     -DTHIRDPARTY=ON \
     -DBUILD_TESTING:BOOL=OFF \
-    -DPYTHON_SOABI="cpython-35m-arm-linux-gnueabihf" \
-    -DEIGEN3_INCLUDE_DIR="${CMAKE_SYSROOT}/usr/include/eigen3" \
-    -DEigen3_DIR="${CMAKE_SYSROOT}/usr/lib/cmake/eigen3" \
-    -DCURL_INCLUDE_DIR="${CMAKE_SYSROOT}/usr/include/${TOOLCHAIN_PREFIX}" \
-    -DCURL_LIBRARY="${CMAKE_SYSROOT}/usr/lib/${TOOLCHAIN_PREFIX}/libcurl.so"
+    -DPYTHON_SOABI="cpython-35m-arm-linux-gnueabihf"
 
